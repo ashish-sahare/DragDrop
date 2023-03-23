d56c65a7a3a025091fb104f7aed4f871bd3a01c7
ashish-sahare <ashishsahare2000@gmail.com>
2023-03-23 23:22:05 +0530

    Association Validation and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+list = List.create(name: "Todo")
+list.tasks.create(name: "record a video")
+list.tasks.create(name: "read a book")
+list.tasks.create(name: "drink water")
+list.tasks.create(name: "eat food")
+
+list = List.create(name: "In Progress")
+list = List.create(name: "Done")
